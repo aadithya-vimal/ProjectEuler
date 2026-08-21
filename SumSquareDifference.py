e726bf09a2739177865c172d59065f276eae641c
Aadithya Vimal <aadithyavimal.work@gmail.com>
2026-08-21 21:12:18 +0530

    Problem 6: Sum Square Difference

---
+++ SumSquareDifference.py
@@ -0,0 +1,9 @@
+# Finding the individual sum of squares and square of sums
+sum_of_squares = 0
+square_of_sums = 0
+for i in range(1,101):
+    sum_of_squares += i**2
+    square_of_sums += i
+square_of_sums = square_of_sums**2
+# Difference between the sum of the squares and square of the sums
+print(square_of_sums - sum_of_squares)
